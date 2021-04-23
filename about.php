592578ddf881fb0b17ca9949f5fef5f209d4ef34
hibasetti <hiba.ouldsetti@gmail.com>
2021-04-24 00:49:46 +0100

    about page

---
+++ about.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="css/bootstrap.min.css" rel="stylesheet"> 
+  <script src="https://kit.fontawesome.com/db5a3da844.js" crossorigin="anonymous"></script>
+  <script src="js/jquery.js"></script>
+   <script src="js/bootstrap.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <link href="css/main.css" rel="stylesheet">
+   
+  <title>Schade 24 | FAQ</title>
+</head>
+<body class="aboutUs">
+  <header>
+  <?php include('navigation.php'); ?>
+
+    <section class="One">
+      <div class="container row">
+        <div class="left-block col-md-5">
+          <h1>
+            About US
+          </h1>
+      
+          </div>
+          <div class="right-block col-md-6">
+          <img src="images/aboutUs.svg" />
+          </div>
+      </div>
+    </section>
+</header>
+
+<section class="shadow services">
+  <div class="container">
+      <h1>Our services</h1>
+      <div class="item row">
+          <div class="left col-md-2">
+              <img src="images/patient.svg" />
+          </div>
+         <div class="right col-md-9">
+            <h4>Personal injury</h4>
+            <p>We help you quickly compare the best moped insurance policies on price and quality. 
+              18pxWe are 100% independent in this! 18pxYou will be on your way insured within 5 minutes.</p>
+            <a href="#" class="btn btn-light rounded-pill purple-button"> Report Damage <i class="fas fa-angle-right"></i></a>
+
+          </div>
+      </div>
+      <div class="item row">
+          <div class="left col-md-2">
+              <img src="images/medical-history.svg" />
+          </div>
+         <div class="right col-md-9">
+            <h4>Personal injury</h4>
+            <p>We help you quickly compare the best moped insurance policies on price and quality. 
+              18pxWe are 100% independent in this! 18pxYou will be on your way insured within 5 minutes.</p>
+            <a href="#" class="btn btn-light rounded-pill purple-button"> Report Damage <i class="fas fa-angle-right"></i></a>
+
+          </div>
+      </div>
+      <div class="item row">
+          <div class="left col-md-2">
+              <img src="images/storm.svg" />
+          </div>
+         <div class="right col-md-9">
+            <h4>Personal injury</h4>
+            <p>We help you quickly compare the best moped insurance policies on price and quality. 
+              18pxWe are 100% independent in this! 18pxYou will be on your way insured within 5 minutes.</p>
+            <a href="#" class="btn btn-light rounded-pill purple-button"> Report Damage <i class="fas fa-angle-right"></i></a>
+
+          </div>
+      </div>
+      <div class="item row">
+          <div class="left col-md-2">
+              <img src="images/delay.svg" />
+          </div>
+         <div class="right col-md-9">
+            <h4>Personal injury</h4>
+            <p>We help you quickly compare the best moped insurance policies on price and quality. 
+              18pxWe are 100% independent in this! 18pxYou will be on your way insured within 5 minutes.</p>
+            <a href="#" class="btn btn-light rounded-pill purple-button"> Report Damage <i class="fas fa-angle-right"></i></a>
+
+          </div>
+      </div>
+      <div class="item row">
+          <div class="left col-md-2">
+              <img src="images/breakdown.svg" />
+          </div>
+         <div class="right col-md-9">
+            <h4>Personal injury</h4>
+            <p>We help you quickly compare the best moped insurance policies on price and quality. 
+              18pxWe are 100% independent in this! 18pxYou will be on your way insured within 5 minutes.</p>
+            <a href="#" class="btn btn-light rounded-pill purple-button"> Report Damage <i class="fas fa-angle-right"></i></a>
+
+          </div>
+      </div>
+      <div class="item row">
+          <div class="left col-md-2">
+              <img src="images/engineer.svg"  />
+          </div>
+         <div class="right col-md-9">
+            <h4>Personal injury</h4>
+            <p>We help you quickly compare the best moped insurance policies on price and quality. 
+              18pxWe are 100% independent in this! 18pxYou will be on your way insured within 5 minutes.</p>
+            <a href="#" class="btn btn-light rounded-pill purple-button"> Report Damage <i class="fas fa-angle-right"></i></a>
+
+          </div>
+      </div>
+  </div>
+</section>
+<section class="shadow testimony">
+    <div class="container-xl">
+      <div class="row">
+        <div class="col-lg-10 mx-auto">
+          
+          <div id="myCarousel" class="carousel slide" data-ride="carousel">
+          
+            <!-- Wrapper for carousel items -->
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <p class="testimonial">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante. Idac bibendum scelerisque non non purus. Suspendisse varius nibh non aliquet.</p>
+                <p class="overview"><b>Paula Wilson</b></p>
+              </div>
+              <div class="carousel-item">
+                <p class="testimonial">Vestibulum quis quam ut magna consequat faucibus. Pellentesque eget nisi a mi suscipit tincidunt. Utmtc tempus dictum risus. Pellentesque viverra sagittis quam at mattis. Suspendisse potenti. Aliquam sit amet gravida nibh, facilisis gravida odio.</p>
+                <p class="overview"><b>Antonio Moreno</b></p>
+              </div>
+              <div class="carousel-item">
+                <p class="testimonial">Phasellus vitae suscipit justo. Mauris pharetra feugiat ante id lacinia. Etiam faucibus mauris id tempor egestas. Duis luctus turpis at accumsan tincidunt. Phasellus risus risus, volutpat vel tellus ac, tincidunt fringilla massa. Etiam hendrerit dolor eget rutrum.</p>
+                <p class="overview"><b>Michael Holz</</p>
+              </div>
+                 <div class="quote">
+                    <img src="images/quotes-purple.png" />
+                   </div>
+            </div>
+            <!-- Carousel controls -->
+            <a class="carousel-control-prev" href="#myCarousel" data-slide="prev">
+              <i class="fa fa-angle-left"></i>
+            </a>
+            <a class="carousel-control-next" href="#myCarousel" data-slide="next">
+              <i class="fa fa-angle-right"></i>
+            </a>
+          
+          </div>
+         
+        </div>
+      </div>
+     
+    </div>
+</section>
+<section class="awards-section">
+	<div class="container row">
+		<div class="col-md-11 award shadow">
+			<div class="stars">
+        <h1>Award <span>2019</span></h1>
+      </div>
+        
+			<p>
+				In 2017, Schade24 took the initiative to build up good relationships with insurers by means
+				of the Schade24 Award and thus improve claims handling of personal injury claims. This year
+				we will not limit the assessed parties to only insurers,
+				but we will also assess all involved personal injury agencies according to various criteria.
+			</p>
+			<div class="button-stars">
+        <a href="#" class="btn btn-light btn-lg rounded-pill"> Award Details</a>
+      </div>
+			<div>
+				<img src="images/awards2.svg" />
+			</div>
+
+		</div>
+	</div>
+</section>
+<section class="Three comparing">
+    <div class="container">
+        <h1>
+        Comparing pays off
+        </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Perspiciatis suscipit quasi inventore sunt modi eaque explicabo, 
+            fugit vero ratione nemo maxime pariatur esse dolore.</p>
+        
+        <a href="#" class="btn btn-light btn-lg rounded-pill orange-button"> Report Damage <i class="fas fa-angle-right"></i></a>
+        
+    </div>
+
+</section>
+
+
+
+ 
+
+<?php include('footer.php'); ?>
+</body>
+</html>
